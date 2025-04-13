ba3acbc3fc84cd14fa791dd79888f19b46e8db1c
zhnd <zhufind@yeah.net>
2025-04-13 21:32:16 +0800

    Refactor navigation styles to use @reference instead of @apply; update global CSS to use @import for Tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,15 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 
-import tailwind from "@astrojs/tailwind";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://outinx.com/",
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [mdx(), sitemap()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
