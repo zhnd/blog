fb46dd8d105c6293cc03e33ec9963bb061073f17
zhnd <zhufind@yeah.net>
2025-04-16 21:37:27 +0800

    refactor content and ci

--- src/utils/data-utils.ts
+++ src/utils/data-utils.ts
@@ -1,8 +1,14 @@
 import { type CollectionEntry } from 'astro:content';
 import { slugify } from './common-utils';
 
-export function sortItemsByDateDesc(itemA: CollectionEntry<'blog' | 'projects'>, itemB: CollectionEntry<'blog' | 'projects'>) {
-    return new Date(itemB.data.publishDate).getTime() - new Date(itemA.data.publishDate).getTime();
+export function sortItemsByDateDesc(
+  itemA: CollectionEntry<"blog">,
+  itemB: CollectionEntry<"blog">
+) {
+  return (
+    new Date(itemB.data.publishDate).getTime() -
+    new Date(itemA.data.publishDate).getTime()
+  );
 }
 
 export function getAllTags(posts: CollectionEntry<'blog'>[]) {
