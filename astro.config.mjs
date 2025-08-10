14969a0bb24e7789647e8bd869dcef54804833b7
zhnd <zhufind@yeah.net>
2025-08-10 18:27:38 +0800

    feat: add new blog posts and optimize layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,23 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: "https://outinx.com",
+  integrations: [mdx(), sitemap()],
+  markdown: {
+    syntaxHighlight: "shiki",
+    shikiConfig: {
+      themes: {
+        light: "github-light",
+        dark: "github-dark",
+      },
+      wrap: true,
+    },
+  },
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
